95b0192ab5456e850d70295f20ece35c8a37d2da
warcry98 <warcry98@gmail.com>
2026-03-15 18:45:36 +0700

    Change code to use pyarrow inside pandas

--- pyflink-project/streaming/src/models.py
+++ pyflink-project/streaming/src/models.py
@@ -1,21 +1,36 @@
 import json
+import math
 from dataclasses import dataclass
+from typing import Optional
 
 @dataclass
 class Ride:
     lpep_pickup_datetime: str
+    lpep_dropoff_datetime: str
     PULocationID: int
     DOLocationID: int
+    passenger_count: Optional[int]
     trip_distance: float
+    tip_amount: float
     total_amount: float
+
+def safe_optional_int(value):
+    if value is None:
+        return None
+    if math.isnan(value):
+        return None
+    return int(value)
 
 def ride_from_row(row):
     return Ride(
-        lpep_pickup_datetime=str(row['lpep_pickup_datetime']),
-        PULocationID=int(row['PULocationID']),
-        DOLocationID=int(row['DOLocationID']),
-        trip_distance=float(row['trip_distance']),
-        total_amount=float(row['total_amount']),
+        lpep_pickup_datetime=str(row.lpep_pickup_datetime),
+        lpep_dropoff_datetime=str(row.lpep_dropoff_datetime),
+        PULocationID=int(row.PULocationID),
+        DOLocationID=int(row.DOLocationID),
+        passenger_count=safe_optional_int(row.passenger_count),
+        trip_distance=float(row.trip_distance),
+        tip_amount=float(row.tip_amount),
+        total_amount=float(row.total_amount),
     )
 
 
--- pyflink-project/streaming/src/producers/producer.py
+++ pyflink-project/streaming/src/producers/producer.py
@@ -1,51 +1,92 @@
 import dataclasses
-import json
 import sys
 import time
 from pathlib import Path
+import urllib.request
+
+import orjson
+import pyarrow.parquet as pq
+from kafka import KafkaProducer
 
 sys.path.insert(0, str(Path(__file__).parent.parent))
 
-import pandas as pd
-from kafka import KafkaProducer
-from models import Ride, ride_from_row
+from models import Ride, safe_optional_int
 
-url = "https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2025-10.parquet"
-columns = [
-    'lpep_pickup_datetime', 
-    'lpep_dropoff_datetime', ''
-    'PULocationID', 
-    'DOLocationID', 
-    'passenger_count',
-    'trip_distance', 
-    'tip_amount',
-    'total_amount',
-    ]
-df = pd.read_parquet(url, columns=columns).head(5)
-print(df)
 
-# def ride_serializer(ride):
-#     ride_dict = dataclasses.asdict(ride)
-#     json_str = json.dumps(ride_dict)
-#     return json_str.encode('utf-8')
+URL = "https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2025-10.parquet"
+LOCAL_FILE = "green_tripdata_2025-10.parquet"
 
-# server = 'localhost:9092'
+COLUMNS = [
+    "lpep_pickup_datetime",
+    "lpep_dropoff_datetime",
+    "PULocationID",
+    "DOLocationID",
+    "passenger_count",
+    "trip_distance",
+    "tip_amount",
+    "total_amount",
+]
 
-# producer = KafkaProducer(
-#     bootstrap_servers=[server],
-#     value_serializer=ride_serializer
-# )
-# t0 = time.time()
+BOOTSTRAP_SERVER = "localhost:9092"
+TOPIC = "green-trips"
 
-# topic_name = 'rides'
 
-# for _, row in df.iterrows():
-#     ride = ride_from_row(row)
-#     producer.send(topic_name, value=ride)
-#     print(f"Sent: {ride}")
-#     time.sleep(0.01)
+def ride_serializer(ride: Ride):
+    return orjson.dumps(dataclasses.asdict(ride))
 
-# producer.flush()
 
-# t1 = time.time()
-# print(f'took {(t1 - t0):.2f} seconds')
+def download_if_needed():
+    if not Path(LOCAL_FILE).exists():
+        print("Downloading parquet file...")
+        urllib.request.urlretrieve(URL, LOCAL_FILE)
+        print("Download finished")
+
+
+producer = KafkaProducer(
+    bootstrap_servers=[BOOTSTRAP_SERVER],
+    value_serializer=ride_serializer,
+    linger_ms=50,
+    batch_size=131072,
+    compression_type="gzip",
+)
+
+
+def process_batch(batch):
+
+    cols = {name: batch[name].to_pylist() for name in COLUMNS}
+    size = len(cols["PULocationID"])
+
+    for i in range(size):
+
+        ride = Ride(
+            lpep_pickup_datetime=str(cols["lpep_pickup_datetime"][i]),
+            lpep_dropoff_datetime=str(cols["lpep_dropoff_datetime"][i]),
+            PULocationID=int(cols["PULocationID"][i]),
+            DOLocationID=int(cols["DOLocationID"][i]),
+            passenger_count=safe_optional_int(cols["passenger_count"][i]),
+            trip_distance=float(cols["trip_distance"][i]),
+            tip_amount=float(cols["tip_amount"][i]),
+            total_amount=float(cols["total_amount"][i]),
+        )
+
+        producer.send(TOPIC, value=ride)
+
+
+if __name__ == "__main__":
+
+    t0 = time.time()
+
+    download_if_needed()
+
+    print("Streaming parquet with PyArrow...")
+
+    parquet_file = pq.ParquetFile(LOCAL_FILE)
+
+    for batch in parquet_file.iter_batches(columns=COLUMNS, batch_size=50000):
+        process_batch(batch)
+
+    producer.flush()
+
+    t1 = time.time()
+
+    print(f"Finished in {(t1 - t0):.2f} seconds")
